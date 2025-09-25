4877ee106fad8c84c52e33d7046d0cd5d2415d27
joancovir <joancovir@gmail.com>
2025-09-25 17:09:44 -0500

    Agrego proyecto base de Spring Boot

---
+++ backend/src/test/java/com/djuancito/reposteria/ReposteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djuancito.reposteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
