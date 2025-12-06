967163fcab27e816fb0754048632db37a2750f25
joancovir <JOANCOVIR@GMAIL.COM>
2025-12-05 23:58:25 -0500

    Update SecurityConfig.java

--- backend/src/main/java/com/djuancito/reposteria/config/SecurityConfig.java
+++ backend/src/main/java/com/djuancito/reposteria/config/SecurityConfig.java
@@ -5,8 +5,8 @@ import com.djuancito.reposteria.servicio.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationConfiguration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration; // ← ESTE ES EL BUENO
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,8 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
+import java.util.List; 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
