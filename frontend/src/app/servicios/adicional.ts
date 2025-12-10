acee62da707fc8b23d9ab4b2f980c787008ace7e
joancovir <JOANCOVIR@GMAIL.COM>
2025-12-09 22:42:44 -0500

    Update adicional.ts

--- frontend/src/app/servicios/adicional.ts
+++ frontend/src/app/servicios/adicional.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Adicional } from '../modelos/adicional';
+import { environment } from '../../../src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdicionalService {
 
-  private apiUrl = 'http://localhost:8080/api/adicionales';
+private apiUrl = environment.apiUrl + '/adicionales'; 
 
   constructor(private http: HttpClient) { }
 
