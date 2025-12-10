809cdee9facb94cc57f33dd4ef69c48867f61316
joancovir <JOANCOVIR@GMAIL.COM>
2025-12-09 22:11:19 -0500

    Update confirmar-pedido.ts

--- frontend/src/app/paginas/confirmar-pedido/confirmar-pedido.ts
+++ frontend/src/app/paginas/confirmar-pedido/confirmar-pedido.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../../src/environments/environment';
+import { environment } from '../../../../src/environments/environment';
 
 @Component({
   selector: 'app-confirmar-pedido',
