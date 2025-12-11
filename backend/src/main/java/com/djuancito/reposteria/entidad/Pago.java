726abc4702de44c49b505dbfc3ad56bf91ac5c64
joancovir <JOANCOVIR@GMAIL.COM>
2025-12-11 02:30:49 -0500

    Update Pago.java

--- backend/src/main/java/com/djuancito/reposteria/entidad/Pago.java
+++ backend/src/main/java/com/djuancito/reposteria/entidad/Pago.java
@@ -5,10 +5,13 @@ import java.time.LocalDateTime;
 import jakarta.persistence.*;
 import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 @Entity
 @Table(name = "Pago")
 public class Pago {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "pagoId")
@@ -19,6 +22,12 @@ public class Pago {
     @JsonIgnore
     private Pedido pedido;
 
+    @Transient
+    @JsonProperty("pedidoId")  // ← Esto hace que aparezca en el JSON
+    public Integer getPedidoId() {
+        return pedido != null ? pedido.getPedidoId() : null;
+    }
+
     @Column(name = "montoAbonado")
     private BigDecimal montoAbonado;
 
@@ -27,20 +36,19 @@ public class Pago {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "tipoPago")
-    private TipoPago tipoPago; 
+    private TipoPago tipoPago;
 
     @Column(name = "comprobanteUrl")
     private String comprobanteUrl;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "metodo")
-    private MetodoPago metodo; 
+    private MetodoPago metodo = MetodoPago.PENDIENTE;  // valor por defecto
 
     @Column(name = "codigoOperacion")
     private String codigoOperacion;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "estado")
-    private EstadoPago estado; 
-    
+    @Column(name = EstadoPago.pendiente_validacion;
+    private EstadoPago estado;
 }
