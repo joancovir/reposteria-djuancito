899146a4d8d2212592250a0423e5a09a749aebf2
joancovir <JOANCOVIR@GMAIL.COM>
2025-12-11 01:48:04 -0500

    Update pago.ts

--- frontend/src/app/modelos/pago.ts
+++ frontend/src/app/modelos/pago.ts
@@ -1,15 +1,18 @@
 
-export type MetodoPago = 'yape' | 'plin' | 'efectivo';
+export type MetodoPago = 'PENDIENTE' | 'yape' | 'plin' | 'efectivo';  // AÑADIDO PENDIENTE
+
 export type EstadoPago = 'pendiente_validacion' | 'validado' | 'rechazado';
+
 export type TipoPago = 'GARANTIA' | 'SALDO' | 'TOTAL';
+
 export interface Pago {
   pagoId: number;
   pedidoId?: number;
   montoAbonado: number;
-  fechaPago: string; 
+  fechaPago: string;
   metodo: MetodoPago;
   codigoOperacion?: string;
   comprobanteUrl?: string;
   estado: EstadoPago;
-  tipoPago?: TipoPago;  
+  tipoPago?: TipoPago;  // <-- ÚNICO, sin duplicado
 }
