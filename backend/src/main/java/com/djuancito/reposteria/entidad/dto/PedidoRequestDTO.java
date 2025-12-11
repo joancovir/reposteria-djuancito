e14cb80d9001826ce3441d6131e99acd4f70accf
joancovir <JOANCOVIR@GMAIL.COM>
2025-12-11 00:57:48 -0500

    Update PedidoRequestDTO.java

--- backend/src/main/java/com/djuancito/reposteria/entidad/dto/PedidoRequestDTO.java
+++ backend/src/main/java/com/djuancito/reposteria/entidad/dto/PedidoRequestDTO.java
@@ -4,13 +4,14 @@
     import java.util.List;
     import lombok.Data;
 
-    @Data
-    public class PedidoRequestDTO {
-        private Integer usuarioId;
-        private String nota;
-        private List<DetalleRequestDTO> detalles;
-        private BigDecimal subtotal;
-        private BigDecimal garantiaPagada;
-        private BigDecimal resto;          
-        private BigDecimal total;  
-    }
+  @Data
+public class PedidoRequestDTO {
+    private Integer usuarioId;
+    private String nota;
+    private List<DetalleRequestDTO> detalles;
+    private BigDecimal subtotal;
+    private BigDecimal garantiaPagada;
+    private BigDecimal resto;
+    private BigDecimal total;
+    private String codigoOperacion;  // ← ESTE ES EL CAMPO QUE FALTABA
+}
