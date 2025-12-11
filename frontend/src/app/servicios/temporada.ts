12c423eb9e701868341f652373a425b6ef6be566
joancovir <JOANCOVIR@GMAIL.COM>
2025-12-10 23:58:40 -0500

    Update temporada.ts

--- frontend/src/app/servicios/temporada.ts
+++ frontend/src/app/servicios/temporada.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Temporada } from '../modelos/temporada'; 
+import { Temporada } from '../modelos/temporada';
+import { environment } from '../../../src/environments/environment'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemporadaService {
-  private apiUrl = 'http://localhost:8080/api/temporada'; 
+  private apiUrl = `${environment.apiUrl}/temporada`; 
 
   constructor(private http: HttpClient) { }
 
   getTemporadasActivas(): Observable<Temporada[]> {
     return this.http.get<Temporada[]>(`${this.apiUrl}/activas`);
   }
-  
+ 
   obtenerTodasLasTemporadas(): Observable<Temporada[]> {
     return this.http.get<Temporada[]>(`${this.apiUrl}/todas`);
   }
-  
+ 
   crearOActualizarTemporada(temporada: Temporada): Observable<Temporada> {
     return this.http.post<Temporada>(this.apiUrl, temporada);
   }
